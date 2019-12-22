de515c94b82feaf63d732ef3f854579a0772e1a8
akbarmaul <lanang0995@gmail.com>
2019-12-22 22:04:29 +0700

    add router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => {
+    
     return (
         <div className="homepage">
             <Directory/>
