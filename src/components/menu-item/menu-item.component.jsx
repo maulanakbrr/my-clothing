805e213b2b3544df00daf86ef2ff05fde13c61f1
akbarmaul <lanang0995@gmail.com>
2019-12-28 10:42:40 +0700

    implemented firebase utils, included ability to store authenticated user into our firestore

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,6 @@ import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
-    
     return (
         <div className= {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div style={{backgroundImage: `url(${imageUrl})`}} className="background-image"></div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,23 +3,48 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const configMyClothing ={
-    apiKey: "AIzaSyBg2mo-UDGTvcrK3_nK0LwOORQMUn372vk",
-    authDomain: "my-clothing-db-am95.firebaseapp.com",
-    databaseURL: "https://my-clothing-db-am95.firebaseio.com",
-    projectId: "my-clothing-db-am95",
-    storageBucket: "my-clothing-db-am95.appspot.com",
-    messagingSenderId: "264492541645",
-    appId: "1:264492541645:web:2eea863f58999263f06b84",
-    measurementId: "G-2HF6413EB9"
-  };
+  apiKey: "AIzaSyBg2mo-UDGTvcrK3_nK0LwOORQMUn372vk",
+  authDomain: "my-clothing-db-am95.firebaseapp.com",
+  databaseURL: "https://my-clothing-db-am95.firebaseio.com",
+  projectId: "my-clothing-db-am95",
+  storageBucket: "my-clothing-db-am95.appspot.com",
+  messagingSenderId: "264492541645",
+  appId: "1:264492541645:web:2eea863f58999263f06b84",
+  measurementId: "G-2HF6413EB9"
+};
 
-  firebase.initializeApp(configMyClothing);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return ;
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+  
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error){
+      console.log('error creating user', error.message)
+    }
+  }
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  return userRef;
+} 
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+firebase.initializeApp(configMyClothing);
 
-  export default firebase;
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
