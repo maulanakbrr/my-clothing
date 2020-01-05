8e01f09d724f63a6b779d330d0e83ca88700e077
akbarmaul <lanang0995@gmail.com>
2020-01-05 22:29:52 +0700

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItem, removeItem, addItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({cartItem: {imageUrl, name, quantity, price}}) => (
-    <div className="checkout-item">
-        <div className="image-container">
-            <img src={imageUrl} alt="item"/>
+const CheckoutItem = ({cartItem, clearItem, removeItem, addItem}) => {
+    const {imageUrl, name, quantity, price} = cartItem;
+    return (
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={imageUrl} alt="item"/>
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span className="value">{quantity}</span>
+                <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className="price">${price * quantity}</span>
+            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10006;</div>
         </div>
-        <span className="name">{name}</span>
-        <span className="quantity">{quantity}x</span>
-        <span className="price">${price * quantity}</span>
-        <div className="remove-button">&#10006;</div>
-    </div>
-);
+    )
+}
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItem(item)),
+    removeItem: item => dispatch(removeItem(item)),
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,35 +7,45 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({cartItems, cartTotal}) => (
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Product</span>
+const CheckoutPage = ({cartItems, cartTotal}) => {
+    console.log(cartItems);
+    return (
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
             </div>
-            <div className="header-block">
-                <span>Description</span>
-            </div>
-            <div className="header-block">
-                <span>Quantity</span>
-            </div>
-            <div className="header-block">
-                <span>Price</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
+            {
+                cartItems.length 
+                    ? cartItems.map(cartItem => (
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    ))
+                    : (
+                        <div className="empty-cart">
+                            <span>Your cart is empty</span>
+                        </div>
+                    )
+            }
+            <div className="total">
+                <span>TOTAL: ${cartTotal}</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem => (
-                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-            ))
-        }
-        <div className="total">
-            <span>TOTAL: ${cartTotal}</span>
-        </div>
-    </div>
-)
+    )
+}
+    
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
